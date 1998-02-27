9946037674b7c84f8979e2b3d3be74314a5b92a0
cvs2git <>
1998-02-27 11:45:57 +0000

    This commit was manufactured by cvs2svn to create tag 'rwarren_EmailLib- 0_39'.

--- RiscOS/Sources/Apps/Citrix/WinStation/src/inc/pddevicep.h
+++
@@ -1,6 +0,0 @@
-
-extern int WFCAPI PdLoad( PDLLLINK );
-
-extern PLIBPROCEDURE PdRFrameDeviceProcedures[];
-extern PLIBPROCEDURE PdCryptDeviceProcedures[];
-extern PLIBPROCEDURE PdModemDeviceProcedures[];
