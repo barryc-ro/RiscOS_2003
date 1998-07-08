01ffae393b74521b584fcc25f1d32563a4842c42
cvs2git <>
1998-07-08 12:35:05 +0000

    This commit was manufactured by cvs2svn to create tag 'MailServ-2_74'.

--- RiscOS/Sources/Apps/Citrix/WinStation/src/inc/vddriverp.h
+++
@@ -1,7 +0,0 @@
-
-extern int WFCAPI VdLoad( PDLLLINK );
-
-extern PLIBPROCEDURE VdTW31DriverProcedures[];
-extern PLIBPROCEDURE VdCdmDriverProcedures[];
-extern PLIBPROCEDURE VdSplDriverProcedures[];
-extern PLIBPROCEDURE VdCpmDriverProcedures[];
