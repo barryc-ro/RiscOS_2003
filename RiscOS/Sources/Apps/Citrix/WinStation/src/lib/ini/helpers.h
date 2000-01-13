2fdedf23696e15013eac8466fbc814e8bd011437
cvs2git <>
2000-01-13 13:02:52 +0000

    This commit was manufactured by cvs2svn to create tag 'RedrawScrn-0_01'.

--- RiscOS/Sources/Apps/Citrix/WinStation/src/lib/ini/helpers.h
+++
@@ -1,9 +0,0 @@
-/* > lib/ini/helpers.h
-
- */
-
-extern int _w_gpps( PCHAR pBuffer, PCHAR pEntry, int cbKey, PCHAR pReturn, int cbReturn, int entry );
-extern UINT _htoi( PCHAR s );
-extern ULONG _htol( PCHAR s );
-
-/* eof lib/ini/helpers.h */
