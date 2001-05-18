11c81ede79e4ec514993f74295ca3f73baec1f39
cvs2git <>
2001-05-18 15:12:33 +0000

    This commit was manufactured by cvs2svn to create tag 'EtherI-3_17'.

--- RiscOS/Sources/Apps/NCFresco/commonsrc/unwind.h
+++
@@ -1,6 +0,0 @@
-/* -*-c-*- */
-
-/* Return the name of the caller function or a dummy '<unknown>' is none is found */
-extern char *procname(int pc);
-/* Return the name of the nth function up the stack, or NULL if the stack does not go that far */
-extern char *caller(int n);
