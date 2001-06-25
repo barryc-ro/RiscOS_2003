6c9f6a43ab986a4474164c2fe13de071aa8e8d81
cvs2git <>
2001-06-25 09:36:09 +0000

    This commit was manufactured by cvs2svn to create tag 'Mailbox-0_25'.

--- RiscOS/Sources/Apps/Citrix/WinStation/src/inc/pddevicep.h
+++
@@ -1,8 +0,0 @@
-
-extern int WFCAPI PdLoad( PDLLLINK );
-
-extern PLIBPROCEDURE PdRFrameDeviceProcedures[];
-extern PLIBPROCEDURE PdCryptDeviceProcedures[];
-extern PLIBPROCEDURE PdModemDeviceProcedures[];
-extern PLIBPROCEDURE PdFrameDeviceProcedures[];
-extern PLIBPROCEDURE PdReliDeviceProcedures[];
