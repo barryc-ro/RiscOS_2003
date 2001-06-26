c046d60f75145c74127dcd7636029d5e31a48dda
cvs2git <>
2001-06-26 18:32:47 +0000

    This commit was manufactured by cvs2svn to create tag 'EtherI-3_22'.

--- RiscOS/Sources/Security/CAM/src/h/card.h
+++
@@ -1,9 +0,0 @@
-#define CARD_CMD_LEN		4
-#define CARD_RESP_LEN		2
-
-int	CardIntAuth(BYTE *plain, int pl_len, int key_num, BYTE *cyphr,
-					int max_len, int *cy_len);
-int	CardReadSrn(BYTE *srn, int buff_len, int *len);
-int	CardOpen(HWND hWnd);
-int	CardOpened();
-int	CardClose();
