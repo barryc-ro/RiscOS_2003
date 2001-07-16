b21550ef285d062c3251696a88ba0475be9093a2
cvs2git <>
2001-07-16 13:27:03 +0000

    This commit was manufactured by cvs2svn to create tag 'PNAMod-0_03'.

--- RiscOS/Sources/Apps/NCFresco/commonsrc/fastfont.h
+++
@@ -1,9 +0,0 @@
-/* fastfont.h */
-
-void fastfont_open( char *buffer, int nBufSize );
-void fastfont_set_colours( wimp_paletteword paper, wimp_paletteword pen,
-                           BOOL antialias );
-void fastfont_setfont( int font );
-void fastfont_paint( char *string, int flags, int x, int y, int strwidth );
-void fastfont_flush(void);
-void fastfont_close(void);
